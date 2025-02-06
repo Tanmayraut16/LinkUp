f3a064fe6aecfcc95c1536a9e5b267f50d887131
Tanmayraut16 <Tanmayraut16@outlook.com>
2025-02-06 14:35:00 +0530

    setup postgress and prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,8 @@
-import { Button } from "@/components/ui/button";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+export default async function Home() {
   return (
     <div className="m-4">
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button>
-            Sign in
-          </Button>
-        </SignInButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-
-      <Button variant={"secondary"}>Click Me</Button>
+      <h1>Home Page</h1>
     </div>
   );
 }
